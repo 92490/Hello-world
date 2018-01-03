cdb86448a419c56851be9cfcae9a62c396bb2896
Serhat Tasdelen <34513971+92490@users.noreply.github.com>
2018-01-03 16:22:13 -0700

    Add files via upload

---
+++ part1a.java
@@ -0,0 +1,14 @@
+
+public class part1a {
+
+/*Programmer: Serhat Tasdelen
+Date created: Dec 26, 2017
+School: Rocky Mountain High School; Fort Collins*/
+
+public static void main(String args[])
+{
+	System.out.println("Hello world");
+	System.out.println("Hello again");
+}
+	
+}
